bf8b3ac2c8221517aa6c1d235da5f5203a31aa13
Daniel Flynn <flynndcs.com>
2021-07-05 21:38:08 -0500

    add product location/quantity metadata

--- src/store/fdb/driver.go
+++ src/store/fdb/driver.go
@@ -20,9 +20,9 @@ var (
 	enc             *gob.Encoder
 )
 
-func encodeKey(exactScope []string) (returnBuffer bytes.Buffer) {
+func encodeKey(exactcategorySequence []string) (returnBuffer bytes.Buffer) {
 	enc = gob.NewEncoder(&buffer)
-	enc.Encode(exactScope)
+	enc.Encode(exactcategorySequence)
 	return buffer
 }
 
@@ -89,8 +89,8 @@ func CheckCredentials(accountName string, username string, password string) (isV
 	return ret.(bool)
 }
 
-func Put(name string, scope []string, value []byte) (didPut bool) {
-	buffer = encodeKey(append(scope, name))
+func Put(name string, categorySequence []string, value []byte) (didPut bool) {
+	buffer = encodeKey(append(categorySequence, name))
 	_, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
 		tr.Set(productSubspace.Pack(tuple.Tuple{buffer.Bytes()}), value)
 		return
@@ -103,8 +103,8 @@ func Put(name string, scope []string, value []byte) (didPut bool) {
 	return true
 }
 
-func GetSingle(name string, scope []string) (value []byte) {
-	buffer = encodeKey(append(scope, name))
+func GetSingle(name string, categorySequence []string) (value []byte) {
+	buffer = encodeKey(append(categorySequence, name))
 	ret, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
 		ret = tr.Get(productSubspace.Pack(tuple.Tuple{buffer.Bytes()})).MustGet()
 		return
@@ -117,12 +117,12 @@ func GetSingle(name string, scope []string) (value []byte) {
 	return ret.([]byte)
 }
 
-func GetAllForScope(scope []string) (repeatedValue []byte) {
-	buffer = encodeKey(scope)
-	endKeyInclusive, errStrinc := fdb.Strinc([]byte(scope[len(scope)-1]))
+func GetAllForCategorySequence(categorySequence []string) (repeatedValue []byte) {
+	buffer = encodeKey(categorySequence)
+	endKeyInclusive, errStrinc := fdb.Strinc([]byte(categorySequence[len(categorySequence)-1]))
 	if errStrinc != nil {
 		buffer.Reset()
-		log.Fatal("Could not get real end key from scope", errStrinc)
+		log.Fatal("Could not get real end key from categorySequence", errStrinc)
 	}
 
 	selectorRange := fdb.SelectorRange{
@@ -146,8 +146,8 @@ func GetAllForScope(scope []string) (repeatedValue []byte) {
 	return values
 }
 
-func ClearSingle(name string, scope []string) (didClear bool) {
-	buffer = encodeKey(append(scope, name))
+func ClearSingle(name string, categorySequence []string) (didClear bool) {
+	buffer = encodeKey(append(categorySequence, name))
 	_, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
 		tr.Clear(productSubspace.Pack(tuple.Tuple{buffer.Bytes()}))
 		return
