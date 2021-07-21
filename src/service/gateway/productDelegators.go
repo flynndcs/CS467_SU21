a2cc9ed1259d579fe3454efa4d3847dec7faf035
Daniel Flynn <flynndcs.com>
2021-07-20 23:44:53 -0500

    revise "schema" of key/values and add indices

--- src/service/gateway/productDelegators.go
+++ src/service/gateway/productDelegators.go
@@ -7,8 +7,8 @@ import (
 	"CS467_SU21/proto/service"
 )
 
-func (s *GatewayServer) GetSingleProduct(ctx context.Context, in *service.ProductIdentifier) (*service.StoredProduct, error) {
-	response, err := productClient.GetSingleProduct(ctx, in)
+func (s *GatewayServer) GetProduct(ctx context.Context, in *service.ProductIdentifier) (*service.StoredProduct, error) {
+	response, err := productClient.GetProduct(ctx, in)
 	if err != nil {
 		log.Fatalln("Failed when sending a message with product client:", err)
 	}
@@ -16,8 +16,8 @@ func (s *GatewayServer) GetSingleProduct(ctx context.Context, in *service.Produc
 	return response, nil
 }
 
-func (s *GatewayServer) GetProductsInRange(ctx context.Context, in *service.GetProductsInRangeRequest) (*service.StoredProducts, error) {
-	response, err := productClient.GetProductsInRange(ctx, in)
+func (s *GatewayServer) GetProducts(ctx context.Context, in *service.GetProductsRequest) (*service.StoredProducts, error) {
+	response, err := productClient.GetProducts(ctx, in)
 	if err != nil {
 		log.Fatalln("Failed when sending a message with product client:", err)
 	}
@@ -25,16 +25,16 @@ func (s *GatewayServer) GetProductsInRange(ctx context.Context, in *service.GetP
 	return response, nil
 }
 
-func (s *GatewayServer) PutSingleProduct(ctx context.Context, in *service.PutSingleProductRequest) (*service.StoredProduct, error) {
-	response, err := productClient.PutSingleProduct(ctx, in)
+func (s *GatewayServer) PutProduct(ctx context.Context, in *service.PutProductRequest) (*service.StoredProduct, error) {
+	response, err := productClient.PutProduct(ctx, in)
 	if err != nil {
 		log.Fatalln("Failed to send", err)
 	}
 	return response, nil
 }
 
-func (s *GatewayServer) ClearSingleProduct(ctx context.Context, in *service.ClearSingleProductMessage) (*service.ClearSingleProductMessage, error) {
-	response, err := productClient.ClearSingleProduct(ctx, in)
+func (s *GatewayServer) ClearProduct(ctx context.Context, in *service.ClearProductMessage) (*service.ClearProductMessage, error) {
+	response, err := productClient.ClearProduct(ctx, in)
 	if err != nil {
 		log.Fatalln("Failed to send", err)
 	}
